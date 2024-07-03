bb6960b3c9ea9744bb3480377f90545f7422d0de
Anil <anilsahu9786@gmail.com>
2024-07-03 15:15:17 +0530

    first commit

--- web_development/FrontEnd/Task/p.js
+++ web_development/FrontEnd/Task/p.js
@@ -1,8 +1,16 @@
+const { sum } = require("lodash")
+
 console.log('This is my first script')
 const obj = {
     height: 5,
     width:10,
     draw: () => this.height*this.width //arow function
 }
-
-console.log(obj.draw())
+function add(a, b){
+    return sum = {
+        sum2(){
+            console('a+b')
+        }
+    }
+}
+console.log(add.sum.sum2)
